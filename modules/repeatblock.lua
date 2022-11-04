07020e208a934982df9a9db71a5c209548ea3d2e
Zemtzov7 <72821250+zmv7@users.noreply.github.com>
2022-11-05 01:11:55 +0500

    filter commands

--- modules/repeatblock.lua
+++ modules/repeatblock.lua
@@ -2,8 +2,20 @@ local modname = minetest.get_current_modname()
 local timeout = minetest.settings:get(modname .. ".repeat_timeout") or 60
 local cache = {}
 
+local blocked_cmds = {
+    ["/me"] = true,
+    ["/msg"] = true,
+    ["/pm"] = true,
+    ["/m"] = true
+    }
 local function on_chat_message(name, message)
     if (cache[name] and cache[name]==message) then
+        if message:sub(1,1) == "/" then
+            local cmd = message:match("/%S+")
+            if not blocked_cmds[cmd] then
+                return false
+            end
+        end
         minetest.chat_send_player(
             name,
             minetest.colorize("red", "sorry, repeat messages are not allowed")
