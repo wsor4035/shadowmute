c933a69bbac5b8a3cbe0aa45c8be949a840fd80e
unknown <24964441+wsor4035@users.noreply.github.com>
2022-11-05 20:48:10 -0400

    fix https://github.com/wsor4035/shadowmute/pull/2

--- modules/repeatblock.lua
+++ modules/repeatblock.lua
@@ -1,7 +1,19 @@
 local cache = {}
 
+local command_whitelist = {
+    ["/spawn"] = true,
+    ["/who"] = true,
+    ["/home"]=true,
+    ["/help"]=true,
+}
+
 local function on_chat_message(name, message)
     if (cache[name] and cache[name]==message) then
+        local command = message:split(" ")[1]
+        if(minetest.registered_chatcommands[command] and command_whitelist[command]) then
+            cache[name] = nil
+            return false
+        end
         minetest.chat_send_player(
             name,
             minetest.colorize("red", "sorry, repeat messages are not allowed")
