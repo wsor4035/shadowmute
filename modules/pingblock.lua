07904b86e4c033cde255dfd62a0206fd0413ef60
unknown <24964441+wsor4035@users.noreply.github.com>
2022-11-05 20:34:55 -0400

    fix https://github.com/wsor4035/shadowmute/issues/1

--- modules/pingblock.lua
+++ modules/pingblock.lua
@@ -1,4 +1,5 @@
 local function on_chat_message(name, message)
+    message = minetest.strip_colors(message)
     local _, count = message:gsub("<@.+>", "")
     local _, count2 = message:gsub("@.+", "")
     if (count and count >= 1) or message:find("discord.gg") or (count2 and count2 >= 1) then
